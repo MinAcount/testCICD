bffcd6518452d31afc9f57bf72417bb023740b9e
xoals4050 <xoals4050@gmail.com>
2024-02-23 09:20:26 +0900

    git에 올라간당

---
+++ TestSpringBoot/src/main/java/com/min/edu/homeController.java
@@ -0,0 +1,18 @@
+package com.min.edu;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class homeController {
+	
+	
+	public String index() {
+		
+		
+		
+		
+		return "/WEB-INF/views/index.jsp";
+	}
+	
+
+}
